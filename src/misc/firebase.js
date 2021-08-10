6c1547a6acdf8807b71236bc73cf9ae9216949aa
atishayjain072 <jain.atishay072@gmail.com>
2021-08-10 17:01:25 +0530

    added signin page and signin with Facebook/Google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyBWRVSM3T6cmXi7DV4VnJB1nI79vXLdIv0',
@@ -11,3 +13,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
